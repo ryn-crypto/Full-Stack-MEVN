7be82cc7a3e5cc2b0e9f347fe20e9c9ece3bf816
Riyan First Tiyanto <riyanfirst13@gmail.com>
2024-01-20 21:49:03 +0700

    learn: Javascript asyncronus

--- Async/script.js
+++ Async/script.js
@@ -1,1 +1,21 @@
-console.log('Hello World');
+// promise
+const contohPromise = () => {
+  return new Promise((resolve, reject) => {
+    resolve(() => {
+      console.log('berhasil')
+    });
+    reject(() => {
+      console.log('gagal')
+    });
+  })
+};
+
+
+// async await
+async function contohAsync() {
+  try {
+    result();
+  } catch (error) {
+    console.log(error)
+  }
+}

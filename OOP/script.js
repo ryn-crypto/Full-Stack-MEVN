e9e224d4b8e159c72ec58fae3556dbdf53b36c74
Riyan First Tiyanto <riyanfirst13@gmail.com>
2024-01-23 21:42:09 +0700

    learn: method in function

---
+++ OOP/script.js
@@ -0,0 +1,26 @@
+const hex = (r, g, b) => {
+  return '#' + (1 << 24 | r << 16 | g << 8 | b).toString(16).slice(1);
+}
+
+const rgb = (r, g, b) => {
+  return `rgb(${r}, ${g}, ${b})`;
+}
+
+function convertColor(r, g, b) {
+  const color = {};
+
+  color.r = r;
+  color.g = g;
+  color.b = b;
+
+  color.rgb = function () {
+    const { r, g, b } = this;
+    return `rgb(${r}, ${g}, ${b})`;
+  }
+
+  color.hex = function () {
+    const { r, g, b } = this;
+    return '#' + (1 << 24 | r << 16 | g << 8 | b).toString(16).slice(1);
+  }
+  return color;
+}

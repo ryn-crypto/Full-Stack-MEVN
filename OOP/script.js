a3a1cc91a854919115984510eac28b2432b60994
Riyan First Tiyanto <riyanfirst13@gmail.com>
2024-01-24 00:12:12 +0700

    learn: OOP Javascript

--- OOP/script.js
+++ OOP/script.js
@@ -1,11 +1,3 @@
-const hex = (r, g, b) => {
-  return '#' + (1 << 24 | r << 16 | g << 8 | b).toString(16).slice(1);
-}
-
-const rgb = (r, g, b) => {
-  return `rgb(${r}, ${g}, ${b})`;
-}
-
 function convertColor(r, g, b) {
   const color = {};
 
@@ -24,3 +16,111 @@ function convertColor(r, g, b) {
   }
   return color;
 }
+
+// function Color(r, g, b) {
+//   this.r = r;
+//   this.g = g;
+//   this.b = b;
+// }
+
+// Color.prototype.rgb = function () {
+//   const { r, g, b } = this;
+//   return `rgb(${r}, ${g}, ${b})`;
+// }
+
+// Color.prototype.hex = function () {
+//   const { r, g, b } = this;
+//   return '#' + (1 << 24 | r << 16 | g << 8 | b).toString(16).slice(1);
+// }
+
+class Color {
+  constructor(r, g, b, name) {
+    this.r = r;
+    this.g = g;
+    this.b = b;
+    this.name = name;
+    this.calcHSL();
+  }
+
+  innerRGB() {
+    const { r, g, b } = this;
+    return `${r}, ${g}, ${b}`;
+  }
+
+  colorName() {
+    return this.name.toUpperCase();
+  }
+
+  rgb() {
+    const { r, g, b } = this;
+    return `rgb(${r}, ${g}, ${b})`;
+  }
+
+  hex() {
+    const { r, g, b } = this;
+    return '#' + (1 << 24 | r << 16 | g << 8 | b).toString(16).slice(1);
+  }
+
+  calcHSL() {
+    let { r, g, b } = this;
+    // Make r, g, and b fractions of 1
+    r /= 255;
+    g /= 255;
+    b /= 255;
+
+    // Find greatest and smallest channel values
+    let cmin = Math.min(r, g, b);
+    let cmax = Math.max(r, g, b);
+    let delta = cmax - cmin;
+    let h = 0;
+    let s = 0;
+    let l = 0;
+
+    // Calculate hue
+    // No difference
+    if (delta == 0) h = 0;
+    // Red is max
+    else if (cmax == r) h = ((g - b) / delta) % 6;
+    // Green is max
+    else if (cmax == g) h = (b - r) / delta + 2;
+    // Blue is max
+    else h = (r - g) / delta + 4;
+
+    h = Math.round(h * 60);
+
+    // Make negative hues positive behind 360°
+    if (h < 0) h += 360;
+
+    // Calculate lightness
+    l = (cmax + cmin) / 2;
+
+    // Calculate saturation
+    s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+
+    // Multiply l and s by 100
+    s = +(s * 100).toFixed(1);
+    l = +(l * 100).toFixed(1);
+
+    this.h = h;
+    this.s = s;
+    this.l = l;
+  }
+
+  hsl() {
+    const { h, s, l } = this;
+    return `hsl(${h}, ${s}%, ${l}%)`;
+  }
+
+  fullSaturation() {
+    const { h, l } = this;
+    return `hsl(${h}, 100%, ${l}%)`;
+  }
+
+  opposite() {
+    const { h, s, l } = this;
+    const newHue = (h + 180) % 360;
+    return `hsl(${newHue}, ${s}%, ${l}%)`;
+  }
+}
+
+const red = new Color(255, 67, 89, 'tomato');

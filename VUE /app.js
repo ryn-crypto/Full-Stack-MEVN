4c0d13f9ee655b79ba4bd5b7658635f67610a393
Riyan First Tiyanto <riyanfirst13@gmail.com>
2024-01-25 22:41:41 +0700

    learn: vite lopping

--- VUE /app.js
+++ VUE /app.js
@@ -1,10 +1,57 @@
-const { createApp, ref } = Vue;
+var dataProduct = {
+  products: [
+    {
+      "id": 1,
+      "name": "star wars",
+      "description": "star wars",
+      "price": 10,
+      "image_title": "star wars",
+      "image": "https://images.unsplash.com/photo-1531907700752-62799b2a3e84?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+      "id": 2,
+      "name": "spiderman",
+      "description": "spiderman",
+      "price": 10,
+      "image_title": "spiderman",
+      "image": "https://images.unsplash.com/photo-1613834926943-9e4ac2945744?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+      "id": 3,
+      "name": "batman",
+      "description": "batman",
+      "price": 10,
+      "image_title": "batman",
+      "image": "https://images.unsplash.com/photo-1613834926943-9e4ac2945744?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+      "id": 4,
+      "name": "superman",
+      "description": "superman",
+      "price": 10,
+      "image_title": "superman",
+      "image": "https://images.unsplash.com/photo-1613834926943-9e4ac2945744?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+      "id": 5,
+      "name": "ironman",
+      "description": "ironman",
+      "price": 10,
+      "image_title": "ironman",
+      "image": "https://images.unsplash.com/photo-1613834926943-9e4ac2945744?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+      "id": 6,
+      "name": "hulk",
+      "description": "hulk",
+      "price": 10,
+      "image_title": "hulk",
+      "image": "https://images.unsplash.com/photo-1613834926943-9e4ac2945744?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    }
+  ]
+};
 
-createApp({
-  setup() {
-    const message = ref("Hello vue!");
-    return {
-      message,
-    };
-  },
-}).mount("#app");
+var app = new Vue({
+  el: '#app',
+  data: dataProduct
+});

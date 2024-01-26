a94551de1c31ebefdbf4cf655102444357379499
Riyan First Tiyanto <riyanfirst13@gmail.com>
2024-01-26 22:40:32 +0700

    learn: Vue

--- VUE /app.js
+++ VUE /app.js
@@ -1,57 +1,22 @@
-var dataProduct = {
-  products: [
-    {
-      "id": 1,
-      "name": "star wars",
-      "description": "star wars",
-      "price": 10,
-      "image_title": "star wars",
-      "image": "https://images.unsplash.com/photo-1531907700752-62799b2a3e84?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-    },
-    {
-      "id": 2,
-      "name": "spiderman",
-      "description": "spiderman",
-      "price": 10,
-      "image_title": "spiderman",
-      "image": "https://images.unsplash.com/photo-1613834926943-9e4ac2945744?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-    },
-    {
-      "id": 3,
-      "name": "batman",
-      "description": "batman",
-      "price": 10,
-      "image_title": "batman",
-      "image": "https://images.unsplash.com/photo-1613834926943-9e4ac2945744?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-    },
-    {
-      "id": 4,
-      "name": "superman",
-      "description": "superman",
-      "price": 10,
-      "image_title": "superman",
-      "image": "https://images.unsplash.com/photo-1613834926943-9e4ac2945744?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-    },
-    {
-      "id": 5,
-      "name": "ironman",
-      "description": "ironman",
-      "price": 10,
-      "image_title": "ironman",
-      "image": "https://images.unsplash.com/photo-1613834926943-9e4ac2945744?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-    },
-    {
-      "id": 6,
-      "name": "hulk",
-      "description": "hulk",
-      "price": 10,
-      "image_title": "hulk",
-      "image": "https://images.unsplash.com/photo-1613834926943-9e4ac2945744?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-    }
-  ]
-};
 
-var app = new Vue({
+
+const app = new Vue({
   el: '#app',
-  data: dataProduct
+  data: {
+    maximum: 50,
+    products: null,
+    cart: [],
+  },
+  mounted: function () {
+    fetch('https://hplussport.com/api/products/order/price')
+      .then(response => response.json())
+      .then(data => {
+        this.products = data;
+      });
+  },
+  methods: {
+    addItem: function (product) {
+      this.cart.push(product);
+    }
+  },
 });

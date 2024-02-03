e0e157f8e348687b6f65b4863932ca62f17b0064
Riyan First Tiyanto <riyanfirst13@gmail.com>
2024-02-03 21:25:10 +0700

    learn: Node and npm

---
+++ NODE JS/module/rumus.js
@@ -0,0 +1,22 @@
+const penjumlahan = (a, b) => a + b;
+
+const pengurangan = (a, b) => a - b;
+
+const perkalian = (a, b) => a * b;
+
+const pembagian = (a, b) => a / b;
+
+const phi = 3.14;
+
+const luasLingkaran = (r) => phi * r * r;
+
+const kelilingLingkaran = (r) => 2 * phi * r;
+
+module.exports = {
+  penjumlahan,
+  pengurangan,
+  perkalian,
+  pembagian,
+  luasLingkaran,
+  kelilingLingkaran
+};
